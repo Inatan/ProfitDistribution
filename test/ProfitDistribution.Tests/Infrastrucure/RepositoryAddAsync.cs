72ed27e4522fc625eb6d71306fef46bd47c917b4
Inatan <inatan_hertzog@hotmail.com>
2021-03-07 13:35:50 -0300

    Added mock Infrastruct tests

---
+++ test/ProfitDistribution.Tests/Infrastrucure/RepositoryAddAsync.cs
@@ -0,0 +1,25 @@
+﻿using Moq;
+using ProfitDistribution.Infrastructure;
+using Xunit;
+
+namespace ProfitDistribution.Tests.Infrastrucure
+{
+    public class RepositoryAddAsync
+    {
+        [Fact]
+        public void AddNewEntity_EntityObjectIsNotNull()
+        {
+            object obj = new object();
+            string key = "000112";
+            var mock = new Mock<IRepository<object>>();
+            var repo = mock.Object;
+
+            //act
+            repo.AddAsync(key, obj);
+
+            //assert
+            var tarefa = repo.FindAsync(key);
+            Assert.NotNull(tarefa);
+        }
+    }
+}
